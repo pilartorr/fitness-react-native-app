14d3eb4f1baffb27e8fac01cb62a281e1e1e3111
pilartorr <sauleturme@gmail.com>
2020-05-19 18:48:50 +0200

    added redux to the app

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
-import { getMetricMetaInfo, timeToString } from "../utils/helpers";
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from "../utils/helpers";
 import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
 import { Ionicons } from "@expo/vector-icons";
 import TextButton from "./TextButton";
 import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -16,7 +18,7 @@ function SubmitBtn({ onPress }) {
   );
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
     state = {
         run: 0,
         bike: 0,
@@ -56,6 +58,9 @@ export default class AddEntry extends Component {
         const entry = this.state;
   
         // Update Redux
+        this.props.dispatch(addEntry({
+            [key]: entry
+        }))
     
         this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
     
@@ -70,6 +75,9 @@ export default class AddEntry extends Component {
         const key = timeToString();
     
         // Update Redux
+        this.props.dispatch(addEntry({
+            [key]: getDailyReminderValue()
+        }))
     
         // Route to Home
     
@@ -97,27 +105,37 @@ export default class AddEntry extends Component {
                     const value = this.state[key];
         
                     return (
-                    <View key={key}>
-                        {getIcon()}
-                        {type === "slider" ? (
-                        <UdaciSlider
-                            value={value}
-                            onChange={value => this.slide(key, value)}
-                            {...rest}
-                        />
-                        ) : (
-                        <UdaciSteppers
-                            value={value}
-                            onIncrement={() => this.increment(key)}
-                            onDecrement={() => this.decrement(key)}
-                            {...rest}
-                        />
-                        )}
-                </View>
-                );
-            })}
-            <SubmitBtn onPress={this.submit} />
-        </View>
-      );
+                        <View key={key}>
+                            {getIcon()}
+                            {type === "slider" ? (
+                            <UdaciSlider
+                                value={value}
+                                onChange={value => this.slide(key, value)}
+                                {...rest}
+                            />
+                            ) : (
+                            <UdaciSteppers
+                                value={value}
+                                onIncrement={() => this.increment(key)}
+                                onDecrement={() => this.decrement(key)}
+                                {...rest}
+                            />
+                            )}
+                        </View>
+                    );
+                })}
+                <SubmitBtn onPress={this.submit} />
+            </View>
+        );
     }
-  }
+}
+
+function mapStateToProps (state) {
+    const key = timeToString()
+
+    return {
+        alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+    }
+}
+  
+export default connect(mapStateToProps)(AddEntry) 
