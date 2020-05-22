1332650141b50ee948314b334451e29717c50739
pilartorr <sauleturme@gmail.com>
2020-05-22 22:05:50 +0200

    added the date title to the each entry

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
 
 class EntryDetail extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Entry Detail - {JSON.stringify(this.props.route.params.entryId)}</Text>
-      </View>
-    )
-  }
+
+    setTitle = (entryId) => {
+        if (!entryId) return;
+
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5, 7)
+        const day = entryId.slice(8)
+
+        this.props.navigation.setOptions({
+            title: `${month}/${day}/${year}`
+        });
+    };
+
+    render() {
+        const {entryId} = this.props.route.params;
+        this.setTitle(entryId);
+        return (
+            <View>
+                <Text>Entry Detail - {JSON.stringify(entryId)}</Text>
+            </View>
+        )
+    }
 }
 
 export default EntryDetail
