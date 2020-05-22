976a8a4095d487de8490c93fccf196e696e880ef
pilartorr <sauleturme@gmail.com>
2020-05-22 21:15:07 +0200

    fixed AppStatusBar Component

--- App.js
+++ App.js
@@ -88,6 +88,7 @@ export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{ flex: 1 }}>
+        <AppStatusBar backgroundColor={purple} barStyle="light-content" />
         <NavigationContainer>
           <MainNav/>
         </NavigationContainer>
